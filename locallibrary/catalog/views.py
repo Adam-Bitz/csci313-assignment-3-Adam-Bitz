9ecec3cce79d64b2d36c8ce22021d5d38cb8271d
Adam-Bitz <adam.bitz@ndsu.edu>
2024-04-08 16:16:29 -0500

    Added visit tracker into views.py and display for page views to index.html

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -21,6 +21,10 @@ def index(request):
 
     num_dune_books = Book.objects.filter(title__contains='dune').count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
@@ -28,6 +32,7 @@ def index(request):
         'num_authors': num_authors,
         'num_genres' : num_genres,
         'num_dune_books' : num_dune_books,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
